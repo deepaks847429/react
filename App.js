f16d3395da34f1f57cebccb8bdf5983a2a9bb646
ambitious_hustler <deepaks9631342845@gmail.com>
2023-05-11 04:47:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import About from "./About";
+
+function App() {
+  return (
+    <>
+    <Navbar />
+      <Hero />
+      <About />
+    </>
+  );
+}
+
+export default App;
---
+++ Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+class Navbar extends React.Component {
+    render() {
+      return (
+        <div className="navbar">
+          <h2>NFTium</h2>
+          <ul>
+            <li>Collection</li>
+            <li>Prices</li>
+            <li>Support</li>
+          </ul>
+        </div>
+      );
+    }
+  }
+export default Navbar;
